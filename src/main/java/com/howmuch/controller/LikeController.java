bf657fccea1a1ee06c703e21252108f9fe805d07
akakss225 <akakss225@naver.com>
2022-01-07 18:25:57 +0900

    01 / 07 오후2 last

--- src/main/java/com/howmuch/controller/LikeController.java
+++ src/main/java/com/howmuch/controller/LikeController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
